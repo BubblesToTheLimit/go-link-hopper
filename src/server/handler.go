0cb4530d5bfddc6868956cb1ad453f99718d90d0
Sebastian Elm <sebastian@applike.info>
2017-07-21 16:42:29 +0200

    added Welcome server

---
+++ src/server/handler.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome, %s", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListendAndServe(":8080", nil)
+}
