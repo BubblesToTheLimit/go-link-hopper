6a0c5b8bc858db1ef58b424f1ce51410d47229a1
BubblesToTheLimit <mond.beton@googlemail.com>
2017-07-21 20:16:58 +0200

    html template working

--- src/server/server.go
+++ src/server/server.go
@@ -3,13 +3,29 @@ package main
 import (
     "fmt"
     "net/http"
+    "html/template"
+    //"net/url"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome, %s", r.URL.Path[1:])
+    // trying to read parameter
+    r.ParseForm()
+    url := r.Form.Get("url")
+
+    if url == "" {
+       //url := "no url found"
+       fmt.Printf("url not found")
+    }
+    
+    t, err := template.ParseFiles("handler.html")
+    if err != nil{
+      panic(err)
+    }
+    t.Execute(w, "static")
 }
 
 func main() {
+    fmt.Printf("Hello, friend")
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
