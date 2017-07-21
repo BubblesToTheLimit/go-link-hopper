282d6937b34031a695434500b02a3707e9bfc2b6
BubblesToTheLimit <mond.beton@googlemail.com>
2017-07-21 21:19:31 +0200

    json result implemented

--- src/server/server.go
+++ src/server/server.go
@@ -3,25 +3,42 @@ package main
 import (
     "fmt"
     "net/http"
-    "html/template"
-    //"net/url"
+    "encoding/json"
 )
+
+type Result struct {
+    Url string
+    Message string
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
     // trying to read parameter
     r.ParseForm()
     url := r.Form.Get("url")
+    message := ""
 
     if url == "" {
-       //url := "no url found"
        fmt.Printf("url not found")
+       message = "please enter a valid url"
+    } else {
+       message = "url received"
     }
-    
-    t, err := template.ParseFiles("handler.html")
-    if err != nil{
-      panic(err)
+
+    myresult := Result{
+                 Url: url,
+                 Message: message,
     }
-    t.Execute(w, "static")
+
+    jData, err := json.Marshal(myresult)
+    if err != nil {
+       fmt.Println("json Marshal parsing error", err)
+       http.Error(w, err.Error(), http.StatusInternalServerError)
+       return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jData)    
+    //fmt.Fprintf(w, string(jData))
 }
 
 func main() {
