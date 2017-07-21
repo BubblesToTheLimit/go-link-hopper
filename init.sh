4654180990f9ca0400c122ec86686d45a2cca819
Sebastian Elm <sebastian@applike.info>
2017-07-21 18:16:49 +0200

    added dep, added chrome headless dev protocol

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-go get -u github.com/golang/dep/cmd/dep
-cd $GOPATH/src/server
-dep ensure
-cd $GOPATH/src/validator
-dep ensure
-cd $GOPATH/src/headless-operator
-dep ensure
---
+++ src/validator/validator.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "browser"
+)
+
+func main() {
+    browser.New()
+}
